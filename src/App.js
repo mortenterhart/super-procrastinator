b12ad963a6b94709e4635b6bad36f7b14374fa01
Michael Skrzypietz <michael.s97@web.de>
2018-02-26 19:27:10 +0100

    added boostrap/reactstrap and example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Button} from 'reactstrap';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 Super Procrastinator
+                <Button color="danger">Danger!</Button>
             </div>
         );
     }
