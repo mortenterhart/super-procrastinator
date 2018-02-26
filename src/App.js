0173a68c30587b4997f8ec0dce45c44c120816af
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-26 20:25:53 +0100

    nicer Way to add a Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import './components/Navbar'
+import Navbar from './components/Navbar'
 class App extends Component {
     render() {
         return (
-
-            <nav class="navbar navbar-light bg-faded">
-                <a class="navbar-brand" href="#">SuperProcrastinator</a>
-                <button type="button" class="btn btn-danger" href="#"> Help</button>
-            </nav>
+            <Navbar render/>
         );
     }
 }
