90b48b97824f6b53973c71c6f3cb70a424b61c5a
Michael Skrzypietz <michael.s97@web.de>
2018-02-27 09:35:17 +0100

    implemented basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
+import {Row, Col} from 'reactstrap';
 import './App.css';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import Navbar from './components/navbar/Navbar'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+
 class App extends Component {
     render() {
         return (
-            <Navbar render/>
+            <div>
+                <Row>
+                    <Navbar/>
+                </Row>
+                <Row className="mainContent">
+                    <Col xs="0" md="3" lg="2" className="contentSelection ">
+                    </Col>
+                    <Col md="9" lg="10" className="contentView">
+                    </Col>
+                </Row>
+                <Row>
+                    <Footer/>
+                </Row>
+            </div>
         );
     }
 }
