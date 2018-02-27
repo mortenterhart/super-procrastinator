de87c338196849147f80ed800bbc814663ce0d6a
Michael Skrzypietz <michael.s97@web.de>
2018-02-27 10:12:58 +0100

    merged new navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import {Row, Col} from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
 import './App.css';
+import NavBar from './components/navbar/NavBar';
+
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="mx-5">
                 <Row>
-                    <Navbar/>
+                    <NavBar/>
                 </Row>
                 <Row className="mainContent">
                     <Col xs="0" md="3" lg="2" className="contentSelection ">
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,51 @@
 import React, {Component} from 'react';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
-class Navbar extends Component {
+import {
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+class NavBar extends Component {
+
     render() {
         return (
-            <nav className="navbar navbar-light bg-faded">
-                <a className="navbar-brand" href="#">SuperProcrastinator</a>
-                <button type="button" className="btn btn-danger"> Help</button>
-            </nav>
+            <div>
+                <Navbar color="faded" light expand="md">
+                    <NavbarBrand href="/">SuperProcrastinator</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="https://github.com/MSkrzypietz/super-procrastinator">Github</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem>
+                                    Settings
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Login
+                                </DropdownItem>
+                                <DropdownItem divider/>
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Navbar>
+            </div>
         );
     }
 }
 
-export default Navbar;
+export default NavBar;
