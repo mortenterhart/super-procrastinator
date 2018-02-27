d2cac5f4d293aede92ba3caed7b8758bb1fe62b1
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-27 09:31:43 +0100

    Reactstrap

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+class NavBar extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <Navbar color="faded" light expand="md">
+                    <NavbarBrand href="/">SuperProcrastinator</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="https://github.com/MSkrzypietz/super-procrastinator">Github</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem>
+                                    Settings
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Login
+                                </DropdownItem>
+                                <DropdownItem divider/>
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
