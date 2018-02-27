13290b11e9b2c6699c0ca86b316d006f3e2c59c6
Michael Skrzypietz <michael.s97@web.de>
2018-02-27 09:35:05 +0100

    updated navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,9 +3,9 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
 class Navbar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-light bg-faded">
-                <a class="navbar-brand" href="#">SuperProcrastinator</a>
-                <button type="button" class="btn btn-danger" href="#"> Help</button>
+            <nav className="navbar navbar-light bg-faded">
+                <a className="navbar-brand" href="#">SuperProcrastinator</a>
+                <button type="button" className="btn btn-danger"> Help</button>
             </nav>
         );
     }
