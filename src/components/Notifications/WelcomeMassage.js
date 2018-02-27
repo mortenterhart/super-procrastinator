0b68f39d2bdc8a9d127602ae00430d7f489d0f32
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-27 11:09:49 +0100

    added close button

--- src/components/Notifications/WelcomeMassage.js
+++ src/components/Notifications/WelcomeMassage.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, Container } from 'reactstrap';
 
 class WelcomeMassage extends Component {
 
     render() {
         return (
             <div>
-                <Jumbotron>
+                <button type="button" className="close" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <Jumbotron fluid>
+
+                    <Container fluid>
                     <h1 className="display-3">Hello, my friend!</h1>
                     <p className="lead">This is a simple way to waist the most valuable thing in your life, your time. This page consumes a diversity of Webservices form the most popular Time Wasting pages</p>
                     <hr className="my-2" />
@@ -14,6 +19,7 @@ class WelcomeMassage extends Component {
                     <p className="lead">
                         <Button color="primary">Learn More</Button>
                     </p>
+                    </Container>
                 </Jumbotron>
             </div>
         );
