07932547119e2e861651d840a15f635d0f09c8ae
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-28 13:37:09 +0100

    Builded a Subscription Page

--- src/App.js
+++ src/App.js
@@ -3,27 +3,17 @@ import {Row, Col} from 'reactstrap';
 import './App.css';
 import NavBar from './components/navbar/NavBar';
 
-import ContentSelection from './components/contentSelection/ContentSelection';
-import WelcomeMessage from './components/notifications/WelcomeMessage';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Footer from './components/footer/Footer';
+import SubscriptionOptions from "./components/subscriptionPage/SubscriptionOptions";
 
 class App extends Component {
     render() {
         return (
             <div className="mx-5">
                 <NavBar/>
-                <Row>
-                    <WelcomeMessage/>
-                </Row>
-                <Row className="mainContent mb-5">
-                    <Col xs="0" md="3" lg="2" className="contentSelection">
-                        <ContentSelection/>
-                    </Col>
-                    <Col md="9" lg="10" className="contentView">
-                    </Col>
-                </Row>
+               <SubscriptionOptions/>
                 <Row>
                     <Footer/>
                 </Row>
