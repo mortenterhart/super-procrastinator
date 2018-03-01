f4e98a67befe3d475c1798dfd59623e3dd8b5eb8
Michael Skrzypietz <michael.s97@web.de>
2018-03-01 10:51:45 +0100

    improved login

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -9,6 +9,7 @@ import {
     NavLink
 } from 'reactstrap';
 import Link from "react-router-dom/es/Link";
+import Login from './Login';
 
 class NavBar extends Component {
 
@@ -26,21 +27,6 @@ class NavBar extends Component {
             isOpen: !this.state.isOpen
         });
     }
-
-    onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
-    };
 
     render() {
         return (
@@ -61,8 +47,10 @@ class NavBar extends Component {
                             <NavItem>
                                 <Link class="nav-link" to="/subscriptions">Subscriptions</Link>
                             </NavItem>
+                            <NavItem>
+                                <Login />
+                            </NavItem>
                         </Nav>
-                        <div class="g-signin2" data-onsuccess={this.onSignIn} data-theme="dark"></div>
                     </Collapse>
                 </Navbar>
             </div>
