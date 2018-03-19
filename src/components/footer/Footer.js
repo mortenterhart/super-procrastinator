4942bce1b5f5ab51ded9e4ca2bad7d1f840aa3b6
Morten Terhart <morten.terhart@gmail.com>
2018-03-19 13:48:58 +0100

    Added Github Logo to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import './Footer.css';
+import githubLogo from '../../images/github-logo.svg';
 
 class Footer extends Component {
     render() {
         return (
             <div className="Footer py-4 navbar-footer-background footer-border">
-                <a className="d-inline mr-5" href="https://github.com/MSkrzypietz/super-procrastinator">Github</a>
-                <p className="d-inline">Copyright © 2018 SuperProcrastinator Inc.</p>
+                <a href="https://github.com/MSkrzypietz/super-procrastinator" target="_blank" rel="noopener noreferrer">
+                    <img alt="Github Logo" src={githubLogo} className="github-logo" width="30" height="30"/>
+                </a>
+                <a className="d-inline mr-5" href="https://github.com/MSkrzypietz/super-procrastinator" target="_blank" rel="noopener noreferrer">Github</a>
+                <p className="d-inline">Copyright &copy; 2018 SuperProcrastinator Inc.</p>
             </div>
         );
     }
