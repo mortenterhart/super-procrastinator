6fbf2cd1d99d7c9e281e11ee1005cc8f53425395
Morten Terhart <M.Terhart@Laudert.de>
2018-04-24 14:17:21 +0200

    Comment the combination of reducers to a single redux storage instance

--- src/storage/ReduxStorage.js
+++ src/storage/ReduxStorage.js
@@ -1,22 +1,24 @@
-import {updateCheckboxState} from './reducers/CheckboxStateReducer';
+import { updateCheckboxState } from "./reducers/CheckboxStateReducer";
 import rootReducer from "../APIs/redditAPI/reducers/rootReducer";
 import ReduxThunk from "redux-thunk";
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let combinedReducers = combineReducers({
-    checkboxList: updateCheckboxState,
-    redditAPI: rootReducer
+  checkboxList: updateCheckboxState,
+  redditAPI: rootReducer
 });
 
+// Create the single redux storage by combining the reducers from the checkboxes
+// and from the Reddit API
 let storage = createStore(
-    combinedReducers,
-    composeEnhancers(applyMiddleware(ReduxThunk))
+  combinedReducers,
+  composeEnhancers(applyMiddleware(ReduxThunk))
 );
 
 storage.subscribe(() => {
-    console.log("State in storage changed!");
-    console.log(storage.getState());
+  console.log("State in storage changed!");
+  console.log(storage.getState());
 });
 
-export {storage};
+export { storage };
